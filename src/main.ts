b53265843404a957b7f6dc8563125f87d68f11c1
Leonard Göhrs <leonard@goehrs.eu>
2024-10-03 00:01:41 +0200

    action: write persist implementation

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,15 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import { exec } from '@actions/exec'
+import { writeFile } from 'node:fs/promises'
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const token: string = core.getInput('token')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    await exec('sudo fstrim /')
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    await writeFile('~/config/persist', token)
   } catch (error) {
-    // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
